fe1c1c152bcd8176aeb9c74fd0f6e064eea92c2d
JerecraftYT-Clickteam <contactojerecraft@gmail.com>
2025-12-05 11:46:06 -0500

    Add login page with styling and validation
    Introduces a login page with HTML, CSS for styling, and JavaScript for basic credential validation. The interface provides user feedback for successful and failed login attempts.

---
+++ CLASE 11/login/login.js
@@ -0,0 +1,16 @@
+function logear(){
+    let usuario = document.getElementById("usuario").value;
+    let contra = document.getElementById("contra").value;
+    let msg = document.getElementById("msg");
+
+    if (usuario == "peter" && contra == "peter"){
+        msg.innerText = "Bienvenido";
+        msg.style.color = "black";
+        msg.style.transition = "color 2.5s easing"
+    }
+    else{
+        msg.innerText = "Datos no validos";
+        msg.style.color = "white";
+        msg.style.transition = "color 2.5s easing"
+    }
+}
